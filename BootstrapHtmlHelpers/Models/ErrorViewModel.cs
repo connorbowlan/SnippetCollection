2b6ab4e9b8f9f3bd796dbacd438a43659e992ee8
Connor Bowlan <connor@cbowlan.com>
2022-07-16 17:16:39 -0700

    Added a .NET Core MVC app for Bootstrap HTML helpers.

---
+++ BootstrapHtmlHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootstrapHtmlHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
