7dce9b2acca2b56eee39352327a9d66932f19077
connorbowlan <connor@cbowlan.com>
2022-09-05 18:52:21 -0700

    Cleaning up Fact component.

--- cat-facts-explorer/src/components/Fact.js
+++ cat-facts-explorer/src/components/Fact.js
@@ -2,25 +2,17 @@ import { useState } from "react";
 import { getSingleFact } from "../apiHelper";
 
 function Fact() {
-     // Required so we can use the useEffect() hook.
      const [fact, setFact] = useState(() => {
           Promise.all([getSingleFact]).then(async () => {
                setFact(await getSingleFact());
           });
      });
 
-     // No need to await this although getSingleFact()
-     // is defined as asynchronous?
-     // Returns a promise.
-
-     // useEffect() is causing the API to hit repeatedly.
      function getNewFact() {
           Promise.all([getSingleFact]).then(async () => {
                setFact(await getSingleFact());
           });
      }
-
-     // Adding [] as a second argument to useEffect() stopped it from hitting repeatedly.
 
      return (
           <div>
